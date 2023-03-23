2aedf59947dccfb9dfe0d64ea9196ff4a93f3bfa
Alexander Shavrov <alexander.shavrov@outlook.com>
2023-03-23 09:54:48 +0300

    String casting algorithms updated

--- include/alef/cast/macro.h
+++ include/alef/cast/macro.h
@@ -1,9 +1,9 @@
-#ifndef ALEF_CAST_STD_CAST_H
-#define ALEF_CAST_STD_CAST_H
+#ifndef ALEF_CAST_MACRO_H
+#define ALEF_CAST_MACRO_H
 
 #define scast(type, value) static_cast<type>(value)
 #define rcast(type, value) reinterpret_cast<type>(value)
 #define ccast(type, value) const_cast<type>(value)
 #define dcast(type, value) dynamic_cast<type>(value)
 
-#endif // ALEF_CAST_STD_CAST_H
+#endif // ALEF_CAST_MACRO_H
